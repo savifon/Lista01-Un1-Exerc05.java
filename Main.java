84a64d1f502deb8e87fd38cf4a40aef83ebbc6f8
Savio Fonseca <savifon1@gmail.com>
2021-03-16 20:31:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/* Escreva um programa em java que receba um nome de arquivo e uma sequência de
+palavras como argumentos na linha de comando, informe se o arquivo existe ou não, caso
+não exista, crie-o e, por fim, escreva neste arquivo a sequência de palavras passadas como
+argumentos. */
+
+class Main {
+  public static void main(String[] args) {
+    
+		
+		
+  }
+}
